d64c6b217acdd40b1ff44dd874f0ec99af782a51
karpov-dev <54440209+karpov-dev@users.noreply.github.com>
2020-05-05 18:24:51 +0300

    Add files via upload

---
+++ A Lower Bound for Sorting/A Lower Bound for Sorting/Model/ArraySort.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace A_Lower_Bound_for_Sorting.Model
+{
+    public static class ArraySort
+    {
+        //сортировка подсчётом.
+        //очень медленная. Когда запустите со стандартными значениями 
+        //будет считать долговато
+        public static int[] CountingSort(int[] array, int maxValue)
+        {
+            //делаем массив на максимальный размер числа, которое может быть у нас в массиве
+            //т.е в будущем будет по одной ячейке на каждое из чисел.
+            int[] count = new int[maxValue];
+
+            //отсортированый массив.
+            int[] sorted = new int[array.Length];
+
+            //проходим по всему массиву. Считываем значение нашего массива. 
+            //потом в ячейку массива count под номером считанного значения нашего исходного
+            //массива добавляем +1.
+            for ( int i = 0; i < array.Length; i++ )
+                count[array[i]]++;
+
+            //складываем i значение массива count с предыдущим 
+            //необходимо будет для поиска позиции, в которую нужно будет поставить элемент из данного для
+            //сортировки массива в сортированый массив.
+            for ( int i = 1; i < count.Length; i++ )
+                count[i] = count[i] + count[i - 1];
+
+            //сортировка массива
+            for ( int i = array.Length - 1; i >= 0; i-- )
+            {
+                //берем значение нашего массива
+                int value = array[i];
+                
+                //при помощи подготовленного заранее массива count находим позицию 
+                //в которую необходимо поставить элемент.
+                //отнимаем -1, т.к массивы нумеруются с нуля.
+                int position = count[value] - 1;
+
+                //собственно записываем значение в найденную позицию.
+                sorted[position] = value;
+
+                //отнимаем -1 для того, чтобы если найдётся элемент с таким же значением
+                //поставить его слева от уже записанного в массив элемента.
+                //например у нас есть две тройки. Первая тройка, например, попадёт в ячейку под номером 
+                //четыре, а вторая тогда, попадет в ячейку под номером три.
+                count[value]--;
+            }
+
+            //естественно, возвращаем отсортированный массив.
+            return sorted;
+        }
+
+        //сортировка вставками.
+        public static int[] InsertionSort(int[] array)
+        {
+            //пробегаемся по всему массиву.
+            for ( var i = 1; i < array.Length; i++ )
+            {
+                var key = array[i]; //содержит значение массива в i ячейке. Необходимо для последующего сравнения
+                var j = i; //содержит значение, с которого необходимо будет начинать сразвнимать.
+
+                //если наш key (значение массива в i ячейке) меньше значение, которое находится левее
+                //тогда меняем местами key и левый элемент.
+                //напрмер 2 1 0 => 1 2 0 => 1 0 2 => 0 1 2.
+                while ( ( j > 1 ) && ( array[j - 1] > key ) )
+                {
+                    //меняем местами
+                    Swap(ref array[j - 1], ref array[j]); //метод обмена элементов (ref - передаем значение по ссылке)
+                    j--; //для проверки следующего левого элемента (т.к. мы уже сместились).
+                }
+                
+                array[j] = key;
+            }
+            //возвращаем отсортированный массив.
+            return array;
+        }
+
+        //сортировка слиянеем
+        public static int[] MergeSort(int[] array)
+        {
+            return MergeSort(array, 0, array.Length - 1);
+        }
+
+
+        private static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if ( lowIndex < highIndex )
+            {
+                var middleIndex = ( lowIndex + highIndex ) / 2;
+
+                //рекурсивно разбиваем массив на подмассивы
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+
+                //когда уже они будут очень маленькие (максимум 2 элемента), сортируем эти два элемента
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
+        }
+
+        private static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex; //левый индекс.
+            var right = middleIndex + 1; //правый индекс.
+
+            //временный массив, в который мы записываем отсортированные значения.
+            var tempArray = new int[highIndex - lowIndex + 1]; 
+            var index = 0; //начальный индекс для временного массива.
+
+            while ( ( left <= middleIndex ) && ( right <= highIndex ) )
+            {
+                //находим меньший элемент и записываем его в начало.
+                if ( array[left] < array[right] )
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                //первый элемент мы нашли, поэтому смещаем индекс временного массива.
+                index++;
+            }
+
+            //в следующих двух циклах мы ищем правый элемент массива 
+            //если сдвинули левую границу - зайдем в этот цикл.
+            for ( int i = left; i <= middleIndex; i++ )
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            //если правую, то в этот.
+            for ( int i = right; i <= highIndex; i++ )
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            //записываем в наш исходный массив.
+            for ( int i = 0; i < tempArray.Length; i++ )
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+
+        //метод обмена элементов
+        private static void Swap(ref int e1, ref int e2)
+        {
+            var temp = e1;
+            e1 = e2;
+            e2 = temp;
+        }
+
+
+        //Я отсортированые массивы не выводил - перевод из int в string занимал много времени, из-за этого на графике 
+        //происходил полный хаос. Отсортированные массивы можно посмотреть через debug (точкой останова) 
+    }
+}
---
+++ A Lower Bound for Sorting/A Lower Bound for Sorting/Model/SortManager.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using A_Lower_Bound_for_Sorting.Service;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+
+namespace A_Lower_Bound_for_Sorting.Model
+{
+    class SortManager : ModelPropertyChanged
+    {
+        private int _step = 15000;
+        private int _arraySize = 1000000;
+        private int _maxValue = 100000000;
+        private int _minValue = 1;
+        private int[] _countingSortedArray;
+        private int[] _insertionSortedArray;
+        private int[] _mergeSortedArray;
+        private ChartValues<ObservablePoint> _countingSortPoints;
+        private ChartValues<ObservablePoint> _insertionSortPoints;
+        private ChartValues<ObservablePoint> _mergeSortPoints;
+
+        public int[] CountingSortedArray
+        {
+            get => _countingSortedArray;
+            set
+            {
+                _countingSortedArray = value;
+                OnPropertyChanged();
+            }
+        }
+        public int[] InsertionSortedArray
+        {
+            get => _insertionSortedArray;
+            set
+            {
+                _insertionSortedArray = value;
+                OnPropertyChanged();
+            }
+        }
+        public int[] MergeSortedArray
+        {
+            get => _mergeSortedArray;
+            set
+            {
+                _mergeSortedArray = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Step
+        {
+            get => _step;
+            set
+            {
+                if(value > 0 && value < 2147483646 )
+                {
+                    _step = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int ArraySize
+        {
+            get => _arraySize;
+            set
+            {
+                if ( value > 0 && value < 2147483646 )
+                {
+                    _arraySize = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int MaxValue
+        {
+            get => _maxValue;
+            set
+            {
+                if(value > 0 && value < 2147483646)
+                {
+                    _maxValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public int MinValue
+        {
+            get => _minValue;
+            set
+            {
+                if ( value > 0 )
+                {
+                    _minValue = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ChartValues<ObservablePoint> CountingSortPoints
+        {
+            get => _countingSortPoints;
+            set
+            {
+                _countingSortPoints = value;
+                OnPropertyChanged();
+            }
+        }
+        public ChartValues<ObservablePoint> InsertionSortPoints
+        {
+            get => _insertionSortPoints;
+            set
+            {
+                _insertionSortPoints = value;
+                OnPropertyChanged();
+            }
+        }
+        public ChartValues<ObservablePoint> MergeSortPoints
+        {
+            get => _mergeSortPoints;
+            set
+            {
+                _mergeSortPoints = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SortManager()
+        {
+            CountingSortPoints = new ChartValues<ObservablePoint>();
+            InsertionSortPoints = new ChartValues<ObservablePoint>();
+            MergeSortPoints = new ChartValues<ObservablePoint>();
+        }
+
+        async public void Run()
+        {
+            CountingSortPoints = new ChartValues<ObservablePoint>();
+            InsertionSortPoints = new ChartValues<ObservablePoint>();
+            MergeSortPoints = new ChartValues<ObservablePoint>();
+            Task[] sortTasks = new Task[3]
+            {
+                new Task(() =>{
+                    int loops = ArraySize / Step;
+                    Stopwatch timer = new Stopwatch();
+                    for ( int i = 0; i < loops; i++ )
+                    {
+                        int stepSize = Step * ( i + 1 );
+                        int[] array = GetRandomArray(stepSize, MaxValue, MinValue);
+                        timer.Start();
+                        CountingSortedArray = ArraySort.CountingSort(array, MaxValue);
+                        timer.Stop();
+                        AddChartPoint(stepSize, timer.ElapsedMilliseconds, CountingSortPoints);
+                        timer.Reset();
+                    }
+                }),
+                new Task(() =>{
+                    int loops = ArraySize / Step;
+                    Stopwatch timer = new Stopwatch();
+                    for ( int i = 0; i < loops; i++ )
+                    {
+                        int stepSize = Step * ( i + 1 );
+                        int[] array = GetRandomArray(stepSize, MaxValue, MinValue);
+                        timer.Start();
+                        MergeSortedArray = ArraySort.MergeSort(array);
+                        timer.Stop();
+                        AddChartPoint(stepSize, timer.ElapsedMilliseconds, MergeSortPoints);
+                        timer.Reset();
+                    }
+                }),
+                new Task(() =>{
+                    int loops = ArraySize / Step;
+                    Stopwatch timer = new Stopwatch();
+                    for ( int i = 0; i < loops; i++ )
+                    {
+                        int stepSize = Step * ( i + 1 );
+                        int[] array = GetRandomArray(stepSize, MaxValue, MinValue);
+                        timer.Start();
+                        InsertionSortedArray = ArraySort.InsertionSort(array);
+                        timer.Stop();
+                        AddChartPoint(stepSize, timer.ElapsedMilliseconds, InsertionSortPoints);
+                        timer.Reset();
+                    }
+                })
+            };
+
+            for(int i = 0; i < sortTasks.Length; i++ )
+            {
+                sortTasks[i].Start();
+            }
+        }
+
+        private void AddChartPoint(int size, long time, ChartValues<ObservablePoint> pointsList)
+        {
+            pointsList.Add(new ObservablePoint(size, time));
+        }
+
+        private static int[] GetRandomArray(int size, int maxValue, int minValue)
+        {
+            Random random = new Random();
+            int[] array = new int[size];
+            for ( int i = 0; i < size; i++ )
+            {
+                array[i] = random.Next(minValue, maxValue); //заполняю значениями от MinValue до MaxValue
+            }
+            return array;
+        }
+    }
+}
